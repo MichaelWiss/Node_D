045c34ea473fdfa60035ac9d46457fc1e26e7bfc
MichaelWiss <michael.wiss@gmail.com>
2017-01-28 13:53:51 -0600

    about

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const hbs = require('hbs');
+
 
 var app = express();
 
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
+
 
 app.get('/', (req, res) => {
 	// res.send('<h1>Hello express!</h1>');
@@ -16,7 +20,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/about', (req, res) => {
-   res.send('About page');
+   res.render('about.hbs');
 });
 
 app.get('/bad', (req, res) => {
@@ -25,4 +29,6 @@ app.get('/bad', (req, res) => {
 	});
 });
 
-app.listen(3000);
+app.listen(3000, ()=> {
+	console.log('Server is up on port 3000');
+});
